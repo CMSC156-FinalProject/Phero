48402a8f08e39cde2df5bf6524d93e3fbcb2f68d
Matthew F. Simpas <fmatthewsimpas@gmail.com>
2026-05-10 01:11:31 +0800

    test: add tests for Counter feature and architecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:phero_app/main.dart';
+import 'package:phero_app/core/di/service_locator.dart';
 
 void main() {
-  testWidgets('App builds smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Counter increment smoke test', (WidgetTester tester) async {
+    locator.setup();
     await tester.pumpWidget(const PheroApp());
 
-    // Verify that our app renders without crashing.
-    expect(find.byType(MaterialApp), findsOneWidget);
+    // Wait for initial load (simulated repository delay)
+    await tester.pumpAndSettle();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpAndSettle();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
