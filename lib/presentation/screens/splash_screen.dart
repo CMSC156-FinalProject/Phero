3f7f743a4849dc84c121fbc5296ec70943283307
Angel May <167489251+janiolangel@users.noreply.github.com>
2026-05-10 23:36:43 +0800

    feat: added light-dark icon

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  bool isDark = false;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: isDark ? const Color(0xFF0D1B2A) : Colors.white,
-      body: Center(
-        child: Image.asset(
-          isDark
-              ? 'assets/images/logo_phero_dark.png'
-              : 'assets/images/logo_phero_light.png',
-          width: 150,
-        ),
+      body: Stack(
+        children: [
+          // Toggle button top right
+          Positioned(
+            top: 15,
+            right: 15,
+            child: IconButton(
+              icon: Icon(
+                isDark ? Icons.wb_sunny : Icons.nightlight_round,
+                color: isDark ? const Color(0xFF2ECC71) : const Color(0xFF5C6E3E),
+                size: 28,
+              ),
+              onPressed: () {
+                setState(() {
+                  isDark = !isDark;
+                });
+              },
+            ),
+          ),
+
+          // Centered logo
+          Center(
+            child: Image.asset(
+              isDark
+                  ? 'assets/images/logo_phero_dark.png'
+                  : 'assets/images/logo_phero_light.png',
+              width: 230,
+            ),
+          ),
+        ],
       ),
     );
   }
